05fb60afb852aac919fb628fb52f3861699c27db
mohammedsobhi <m.mohamed.sobhi.98@gmail.com>
2022-05-30 16:37:25 +0200

    feat: make variables file globaly seen

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/style/_variables.scss";`,
+      },
+    },
+  },
+};
