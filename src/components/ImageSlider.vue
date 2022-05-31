f56d6d04196a676a15d19d88c98e6e42faa8eac8
mohammedsobhi <m.mohamed.sobhi.98@gmail.com>
2022-05-31 13:23:13 +0200

    feat: create the images slider

---
+++ src/components/ImageSlider.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="slider-item">
+    <img :src="currentData.image" alt="mobile illustration" class="slider-img">
+    <p class="slider-text">{{ currentData.text }}</p>
+    <div class="slider-dots">
+      <span
+        v-for="(item, index) in 3"
+        :key="item"
+        class="slider-dot"
+        :class="{ active: index === currentSlider }"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ImageSlider",
+    data() {
+      return {
+        currentSlider: 0, 
+        sliderData: [
+          {
+            image:require("@/assets/slider-1.svg"),
+            text: "Accelerate Your Entire Mobile Team Workflow",
+          },
+          {
+            image:require("@/assets/slider-2.svg"),
+            text: "The Most Comprehensive Bug Reporting Tool for Mobile Apps",
+          },
+          {
+            image:require("@/assets/slider-3.svg"),
+            text: "Secure Crash Reporting With Real-Time Alerts",
+          },
+        ],
+      }
+    },
+    computed: {
+      currentData() {
+        return this.sliderData[this.currentSlider];
+      },
+    },
+    mounted () {
+      setInterval(() => {
+        this.currentSlider = (this.currentSlider + 1) % 3;
+      }, 2500);
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+.slider-item {
+  text-align: center;
+
+  .slider-img {
+    margin: 0 auto;
+  }
+
+  .slider-text {
+    color: $white-text;
+    font-size: $text-large;
+    margin: $space-x-s 0;
+  }
+
+  .slider-dot {
+    display: inline-block;
+    width: $space-s;
+    height: $space-s;
+    border: 1px solid $white-text;
+    border-radius: 50%;
+    margin: 0 $space-xxs;
+    &.active {
+      background-color: $white-text;
+    }
+  }
+}
+
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,55 @@
 <template>
-  <h1>This is the Login View</h1>
+  <div class="login-container">
+    <section class="img-slider">
+      <image-slider/>
+    </section>
+    <section class="form-container">
+      <header>
+        <div class="logo">
+          <!-- <img src="" alt=""> -->
+        </div>
+        <h1 class="title">Log in to Instabug</h1>
+      </header>
+      <!-- social links (ul>li>a) -->
+      <!-- or divider -->
+      <!-- login form -->
+      <!-- login-footer -->
+    </section>
+  </div>
 </template>
 
 <script>
+import ImageSlider from '@/components/ImageSlider.vue';
+
 export default {
   name: "LoginView",
+  components: {
+    ImageSlider,
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+  .login-container {
+    height: 100vh;
+    display: flex;
+  }
+
+  .form-container,
+  .img-slider {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: $space-xx-s;
+  }
+
+  .img-slider {
+  background-color: $blue-background;
+
+    /* hide image slider on small screens */
+    @media (max-width: 760px) {
+      display: none;
+    }
+  }
+</style>
